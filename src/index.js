95ce8160c2195b3b73806440569cb409755a519e
Shunya Shishido <arerreee@gmail.com>
2019-11-23 03:20:12 +0900

    Zero padding

--- src/index.js
+++ src/index.js
@@ -18,15 +18,19 @@ const getCheckDigit = code => {
   return 10 - (total.toString(10).slice(-1));
 };
 
+
 // https://www.dsri.jp/code/jan_periodical_publication/
 // https://ja.wikipedia.org/wiki/%E9%9B%91%E8%AA%8C%E3%82%B3%E3%83%BC%E3%83%89#%E5%AE%9A%E6%9C%9F%E5%88%8A%E8%A1%8C%E7%89%A9%E3%82%B3%E3%83%BC%E3%83%89
 const create = ({magazineCode, year, month}) => {
   const flag = 491
   const reserved = 0
   const yearLastDigit = year.toString(10).slice(-1)
-  const fragment = `${flag}${reserved}${magazineCode}${month}${yearLastDigit}`;
+  const _month = ('00' + month).slice(-2)
+  const fragment = `${flag}${reserved}${magazineCode}${_month}${yearLastDigit}`;
   const checkDigit = getCheckDigit(fragment);
 
+  console.log(fragment)
+  console.log(checkDigit)
   return parseInt(`${fragment}${checkDigit}`);
 };
 
