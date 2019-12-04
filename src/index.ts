0037f93e6d206f45ae688ce086fc0e6305cc4e65
Shunya Shishido <arerreee@gmail.com>
2019-12-05 02:57:46 +0900

    Change the type of argument to be string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-// https://www.dsri.jp/jan/check_digit.html
-const getCheckDigit = (code:Number) => {
-  // 13 digits
-  const reversed = code.toString(10).split('').reverse();
+/**
+ * Calculate check digit from JAN code, which is 13 digit format
+ * https://www.dsri.jp/jan/check_digit.html
+ * @param code 
+ */
+const getCheckDigit = (code:String) => {
+  const reversed = code.split('').reverse();
   const [even, odd] = reversed.reduce((prev, current, index) => {
     const num = parseInt(current)
     if (index % 2 === 0) {
@@ -15,29 +18,40 @@ const getCheckDigit = (code:Number) => {
 
   const total = even * 3 + odd;
 
-  return 10 - parseInt(total.toString(10).slice(-1));
+  return (10 - parseInt(total.toString(10).slice(-1))).toString(10);
 };
 
 
-// https://www.dsri.jp/code/jan_periodical_publication/
-// https://ja.wikipedia.org/wiki/%E9%9B%91%E8%AA%8C%E3%82%B3%E3%83%BC%E3%83%89#%E5%AE%9A%E6%9C%9F%E5%88%8A%E8%A1%8C%E7%89%A9%E3%82%B3%E3%83%BC%E3%83%89
-const create = (magazineCode: Number, year: Number, month: Number) => {
+/**
+ * Create JAN code from any publication date.
+ * https://www.dsri.jp/code/jan_periodical_publication/
+ * https://ja.wikipedia.org/wiki/%E9%9B%91%E8%AA%8C%E3%82%B3%E3%83%BC%E3%83%89#%E5%AE%9A%E6%9C%9F%E5%88%8A%E8%A1%8C%E7%89%A9%E3%82%B3%E3%83%BC%E3%83%89
+ * 
+ * @param periodicPublicationCode 
+ * @param year 
+ * @param month 
+ */
+const create = (periodicPublicationCode: String, year: Number, month: Number) => {
   const flag = 491
   const reserved = 0
   const yearLastDigit = year.toString(10).slice(-1)
   const _month = ('00' + month).slice(-2)
-  const fragment = `${flag}${reserved}${magazineCode}${_month}${yearLastDigit}`;
-  const checkDigit = getCheckDigit(parseInt(fragment));
+  const fragment = `${flag}${reserved}${periodicPublicationCode}${_month}${yearLastDigit}`;
+  const checkDigit = getCheckDigit(fragment);
 
-  return parseInt(`${fragment}${checkDigit}`);
+  return `${fragment}${checkDigit}`;
 };
 
-const extractMagazineCode = (janCode:Number) => {
-  const match = janCode.toString().match(/^4910(\d{5})\d+$/);
+/**
+ * Extract periodical publication code from JAN code.
+ * @param janCode 
+ */
+const getPeriodicalPublicationCode = (janCode:String) => {
+  const match = janCode.match(/^4910(\d{5})\d+$/);
   return (match && match.length > 1) ? match[1] : null;
 };
 
 export {
   create,
-  extractMagazineCode
+  getPeriodicalPublicationCode
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,10 @@
-import {create, extractMagazineCode} from '../src/index';
+import {create, getPeriodicalPublicationCode} from '../src/index';
 
-describe('JAN code', () => {
-  it ('expects', () => {
-    expect(create(18029, 2019, 8)).toBe(4910180290893)
+describe('Create JAN code ', () => {
+  it ('checks if valid JAN code is outputted', () => {
+    expect(create('18029', 2019, 8)).toBe('4910180290893')
+  })
+  it ('checks if periodical publication code is found from JAN code', () => {
+    expect(getPeriodicalPublicationCode('4910024711294')).toBe('02471')
   })
 })
