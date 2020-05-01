516ad826638c973b5ce5344f51baf913c624f5e0
Shunya Shishido <arerreee@gmail.com>
2020-05-01 23:40:41 +0900

    Update interface

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,44 @@
 type MagazineInfo = {
-  code: number,
-  year: number,
-  month: number
-}
+  code: number;
+  year: number;
+  month: number;
+};
 
 const zeroPadding = (num: number, digit: number): string => {
-  let padd = ''
+  let padd = "";
   for (var i = 0; i < digit; i++) {
-    padd += '0'
+    padd += "0";
   }
-  return (padd + num.toString()).slice(-digit)
-}
+  return (padd + num.toString()).slice(-digit);
+};
 
-const getLastDigit = (num: number): number => parseInt(num.toString(10).slice(-1))
+const getLastDigit = (num: number): number =>
+  parseInt(num.toString(10).slice(-1));
 
 /**
  * Calculate check digit from JAN code, which is 13 digit format
  * https://www.dsri.jp/jan/check_digit.html
- * @param code 
+ * @param code
  */
-const getCheckDigit = (code:string) => {
-  const reversed = code.split('').reverse();
-  const [even, odd] = reversed.reduce((prev, current, index) => {
-    const num = parseInt(current)
-    if (index % 2 === 0) {
-      prev[0] += num;
-    } else {
-      prev[1] += num
-    }
+const getCheckDigit = (code: string): string => {
+  const reversed = code.split("").reverse();
+  const [even, odd] = reversed.reduce(
+    (prev, current, index) => {
+      const num = parseInt(current);
+      if (index % 2 === 0) {
+        prev[0] += num;
+      } else {
+        prev[1] += num;
+      }
 
-    return prev;
-  }, [0, 0]);
+      return prev;
+    },
+    [0, 0]
+  );
 
   const total = even * 3 + odd;
 
-  return (10 - parseInt(total.toString(10).slice(-1))).toString(10);
+  return (10 - getLastDigit(total)).toString(10);
 };
 
 /**
@@ -43,30 +47,27 @@ const getCheckDigit = (code:string) => {
  * https://ja.wikipedia.org/wiki/%E9%9B%91%E8%AA%8C%E3%82%B3%E3%83%BC%E3%83%89#%E5%AE%9A%E6%9C%9F%E5%88%8A%E8%A1%8C%E7%89%A9%E3%82%B3%E3%83%BC%E3%83%89
  * @param magazine
  */
-const create = (magazine: MagazineInfo): string => {
-  const flag = 491
-  const reserved = 0
+const create = (magazine: MagazineInfo): number => {
+  const flag = 491;
+  const reserved = 0;
 
-  const yearLastDigit = getLastDigit(magazine.year)
-  const month = zeroPadding(magazine.month, 2)
-  const code = zeroPadding(magazine.code, 5)
+  const yearLastDigit = getLastDigit(magazine.year);
+  const month = zeroPadding(magazine.month, 2);
+  const code = zeroPadding(magazine.code, 5);
 
   const fragment = `${flag}${reserved}${code}${month}${yearLastDigit.toString()}`;
   const checkDigit = getCheckDigit(fragment);
 
-  return `${fragment}${checkDigit}`;
+  return parseInt(`${fragment}${checkDigit}`);
 };
 
 /**
  * Extract magazine code from JAN code.
  * @param janCode
  */
-const getMagazineCode = (janCode:string): string|null => {
-  const match = janCode.match(/^4910(\d{5})\d+$/);
-  return (match && match.length > 1) ? match[1] : null;
+const getMagazineCode = (janCode: number): number | null => {
+  const match = janCode.toString(10).match(/^4910(\d{5})\d+$/);
+  return match && match.length > 1 ? parseInt(match[1]) : null;
 };
 
-export {
-  create,
-  getMagazineCode
-};
+export { create, getMagazineCode };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,15 @@
-import {create, getPeriodicalPublicationCode} from '../src/index';
+import { create, getMagazineCode } from "../src/index";
 
-describe('Create JAN code ', () => {
-  it ('checks if valid JAN code is outputted', () => {
-    expect(create('18029', 2019, 8)).toBe('4910180290893')
-  })
-  it ('checks if periodical publication code is found from JAN code', () => {
-    expect(getPeriodicalPublicationCode('4910024711294')).toBe('02471')
-  })
-})
+describe("Create JAN code ", () => {
+  it("checks if valid JAN code is outputted", () => {
+    const params = {
+      code: 18029,
+      year: 2019,
+      month: 8,
+    };
+    expect(create(params)).toBe(4910180290893);
+  });
+  it("checks if periodical publication code is found from JAN code", () => {
+    expect(getMagazineCode(4910024711294)).toBe(2471);
+  });
+});
