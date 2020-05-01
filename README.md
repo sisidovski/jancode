321bac97a1fa9a6cf5c3291b3d5576431c611cb2
Shunya Shishido <arerreee@gmail.com>
2020-05-01 23:30:14 +0900

    WIP

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 Utilities functions for Japanese Article Number code (JANコード)
 
-- Extract a periodical publication code (定期刊行物コード) from JAN code.
-- Create JAN code from a periodical publication code and arbital published year and date.
-
 ## Installation
 
 ```
@@ -17,10 +14,10 @@ npm install -d jancode
 const jancode = require('jancode');
 
 // Output JAN code
-jancode.create({'32433', 2019, 3});
-// => "4910324330393"
+jancode.create({32433, 2019, 3});
+// => 4910324330393
 
-// Extract periodical publication code from JAN code
+// Extract a magazine code from JAN code
 jancode.getPeriodicalPublicationCode('4910234234')
 // => '32433'
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+type MagazineInfo = {
+  code: number,
+  year: number,
+  month: number
+}
+
+const zeroPadding = (num: number, digit: number): string => {
+  let padd = ''
+  for (var i = 0; i < digit; i++) {
+    padd += '0'
+  }
+  return (padd + num.toString()).slice(-digit)
+}
+
+const getLastDigit = (num: number): number => parseInt(num.toString(10).slice(-1))
+
 /**
  * Calculate check digit from JAN code, which is 13 digit format
  * https://www.dsri.jp/jan/check_digit.html
  * @param code 
  */
-const getCheckDigit = (code:String) => {
+const getCheckDigit = (code:string) => {
   const reversed = code.split('').reverse();
   const [even, odd] = reversed.reduce((prev, current, index) => {
     const num = parseInt(current)
@@ -21,37 +37,36 @@ const getCheckDigit = (code:String) => {
   return (10 - parseInt(total.toString(10).slice(-1))).toString(10);
 };
 
-
 /**
- * Create JAN code from any publication date.
+ * Create JAN code from any publication date wtih magazine code
  * https://www.dsri.jp/code/jan_periodical_publication/
  * https://ja.wikipedia.org/wiki/%E9%9B%91%E8%AA%8C%E3%82%B3%E3%83%BC%E3%83%89#%E5%AE%9A%E6%9C%9F%E5%88%8A%E8%A1%8C%E7%89%A9%E3%82%B3%E3%83%BC%E3%83%89
- * 
- * @param periodicPublicationCode 
- * @param year 
- * @param month 
+ * @param magazine
  */
-const create = (periodicPublicationCode: String, year: Number, month: Number) => {
+const create = (magazine: MagazineInfo): string => {
   const flag = 491
   const reserved = 0
-  const yearLastDigit = year.toString(10).slice(-1)
-  const _month = ('00' + month).slice(-2)
-  const fragment = `${flag}${reserved}${periodicPublicationCode}${_month}${yearLastDigit}`;
+
+  const yearLastDigit = getLastDigit(magazine.year)
+  const month = zeroPadding(magazine.month, 2)
+  const code = zeroPadding(magazine.code, 5)
+
+  const fragment = `${flag}${reserved}${code}${month}${yearLastDigit.toString()}`;
   const checkDigit = getCheckDigit(fragment);
 
   return `${fragment}${checkDigit}`;
 };
 
 /**
- * Extract periodical publication code from JAN code.
- * @param janCode 
+ * Extract magazine code from JAN code.
+ * @param janCode
  */
-const getPeriodicalPublicationCode = (janCode:String) => {
+const getMagazineCode = (janCode:string): string|null => {
   const match = janCode.match(/^4910(\d{5})\d+$/);
   return (match && match.length > 1) ? match[1] : null;
 };
 
 export {
   create,
-  getPeriodicalPublicationCode
+  getMagazineCode
 };
